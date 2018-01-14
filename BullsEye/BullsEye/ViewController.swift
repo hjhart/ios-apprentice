e4efb122e720a96eba6da160a9cd6c62edee21dc
James Hart <james@wanelo.com>
2018-01-13 17:11:11 -0800

    Reformat code to two spaces. Working game.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,24 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func ShowAlert() {
-        let alert = UIAlertController(title: "Hello, Butts.", message: "Butts butts butts", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome Sauce", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-
+  
+  var currentValue: Int = 50
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func SliderMoved(_ slider: UISlider) {
+    currentValue = lroundf(slider.value)
+  }
+  @IBAction func ShowAlert() {
+    let message = "The value of the slider is: \(currentValue)"
+    let alert = UIAlertController(title: "Hello, Butts.", message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: "Right-o!", style: .default, handler: nil)
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
+  
 }
 
 
