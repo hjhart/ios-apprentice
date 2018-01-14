643421a693d516ca1d738c0b195ce8d76eb3bbad
James Hart <james@wanelo.com>
2018-01-14 15:02:31 -0800

    End of chapter four

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -11,10 +11,25 @@ import UIKit
 class ViewController: UIViewController {
   
   var currentValue: Int = 50
+  var targetValue: Int = 0
+  @IBOutlet weak var targetLabel: UILabel!
+  @IBOutlet weak var slider: UISlider!
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    startRound()
+  }
+  
+  func startRound() {
+    currentValue = 50
+    slider.value = Float(currentValue)
+    targetValue = 1 + Int(arc4random_uniform(100))
+    updateLabels()
+  }
+  
+  func updateLabels() {
+    targetLabel.text = String(targetValue)
   }
   
   override func didReceiveMemoryWarning() {
@@ -25,12 +40,16 @@ class ViewController: UIViewController {
   @IBAction func SliderMoved(_ slider: UISlider) {
     currentValue = lroundf(slider.value)
   }
+  
   @IBAction func ShowAlert() {
-    let message = "The value of the slider is: \(currentValue)"
+    let message = "The value of the slider is: \(currentValue)" +
+                  "\nThe target value is: \(targetValue)"
     let alert = UIAlertController(title: "Hello, Butts.", message: message, preferredStyle: .alert)
     let action = UIAlertAction(title: "Right-o!", style: .default, handler: nil)
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
+    startRound()
+
   }
   
 }
