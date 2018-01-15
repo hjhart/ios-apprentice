7947144f50be265bf3c27be64702bb2ab867777d
James Hart <james@wanelo.com>
2018-01-15 10:32:20 -0800

    Table view

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -20,6 +20,28 @@ class ChecklistViewController: UITableViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+    let label = cell.viewWithTag(1000) as! UILabel
+    
+    if indexPath.row % 5 == 0 {
+      label.text = "Walk the dog"
+    } else if indexPath.row % 5 == 1 {
+      label.text = "Brush my teeth"
+    } else if indexPath.row % 5 == 2 {
+      label.text = "Learn iOS development"
+    } else if indexPath.row % 5 == 3 {
+      label.text = "Soccer pratice"
+    } else if indexPath.row % 5 == 4 {
+      label.text = "Eat some ice cream"
+    }
+    
+    return cell
+  }
 
 }
 
