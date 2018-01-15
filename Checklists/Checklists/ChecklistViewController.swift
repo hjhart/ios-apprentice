0d1e4a4fdd14efb8ef0d82c09099851623d92c95
James Hart <james@wanelo.com>
2018-01-14 23:47:26 -0800

    Check in new Checklists project

---
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Wanelo Engineering on 1/14/18.
+//  Copyright © 2018 Hart, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
