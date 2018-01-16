9ea9542eeac3f530f483dda71b81a6349fdbacc1
James Hart <james@wanelo.com>
2018-01-16 11:57:16 -0800

    Refactor. Finish with Chapter 15.

--- Checklists/Checklists/ItemDetailViewController.swift
+++ Checklists/Checklists/ItemDetailViewController.swift
@@ -8,31 +8,31 @@
 
 import UIKit
 
-protocol AddItemViewControllerDelegate: class {
-  func addItemViewControllerDidCancel(_ controller: AddItemViewController)
-  func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
-  func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem)
+protocol ItemDetailViewControllerDelegate: class {
+  func itemDetailViewControllerDidCancel(_ controller: ItemDetailViewController)
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: ChecklistItem)
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem)
 }
 
-class AddItemViewController: UITableViewController, UITextFieldDelegate {
+class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
   @IBOutlet weak var textField: UITextField!
   @IBOutlet weak var doneBarButton: UIBarButtonItem!
-  weak var delegate: AddItemViewControllerDelegate?
+  weak var delegate: ItemDetailViewControllerDelegate?
   var itemToEdit: ChecklistItem?
 
   @IBAction func cancel() {
-    delegate?.addItemViewControllerDidCancel((self))
+    delegate?.itemDetailViewControllerDidCancel((self))
   }
 
   @IBAction func done() {
     if let itemToEdit = itemToEdit {
       itemToEdit.text = textField.text!
-      delegate?.addItemViewController(self, didFinishEditing: itemToEdit)
+      delegate?.itemDetailViewController(self, didFinishEditing: itemToEdit)
     } else {
       let item = ChecklistItem()
       item.text = textField.text!
       item.checked = false
-      delegate?.addItemViewController(self, didFinishAdding: item)
+      delegate?.itemDetailViewController(self, didFinishAdding: item)
     }
   }
 
